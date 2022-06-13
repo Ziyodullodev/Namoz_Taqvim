95ba053c40edb56d30c431b13ef45c1aa84c998c
Ziyodullo Aliyev <89968839+Ziyodullodev@users.noreply.github.com>
2022-06-13 17:04:26 +0500

    dsda

--- Taqvim/views.py
+++ Taqvim/views.py
@@ -16,7 +16,7 @@ def Vaqtol(viloyat):
     }
     # url = "http://campanulaceae.myspecies.info/"
 
-    response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'})
+    response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     
     # get_responce = requests.get(url)
