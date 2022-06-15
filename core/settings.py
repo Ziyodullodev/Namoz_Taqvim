ba04972da94b25b30c4149317c444b1c6bbec474
Ziyodullo Aliyev <89968839+Ziyodullodev@users.noreply.github.com>
2022-06-15 03:43:43 -0700

    Cors yangiladim

--- core/settings.py
+++ core/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Taqvim',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    # for corse
+    'corsheaders.middleware.CorsMiddlewere',
+    'django.middlewere.common.CommonMiddlewere',
 ]
 
 ROOT_URLCONF = 'core.urls'
@@ -123,3 +128,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ALLOWED_ORIGINS = True
+
+CORS_ALLOWED_CREDENTIALS = True
+
+CORS_ALLOW_METHODS = [
+    "GET",
+]
