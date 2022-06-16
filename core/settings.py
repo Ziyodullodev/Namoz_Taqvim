acb37d1abbf0395e6e4f9dc63e60540a3fcbb106
Ziyodullo Aliyev <89968839+Ziyodullodev@users.noreply.github.com>
2022-06-15 21:14:19 -0700

    add static root

--- core/settings.py
+++ core/settings.py
@@ -124,6 +124,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+if not DEBUG:
+    STATIC_ROOT = BASE_DIR / 'static'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
